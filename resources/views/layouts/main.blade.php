1b428995df9b58feb6e3b164756d2f08d58a4bdb
Rian Adriansyah <43256694+RianAdriansyah@users.noreply.github.com>
2022-07-02 12:07:54 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
